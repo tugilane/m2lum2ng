60b2d83968f4079077f3039b75171a64d52799ef
tugilane <marten.peljo@gmail.com>
2023-11-21 00:17:58 +0200

    näidis arendus 2

--- praktilinetoo näidis/javascript.js
+++ praktilinetoo näidis/javascript.js
@@ -24,9 +24,63 @@ function shuffleArray(array) {
   // Kaardi HTML
 function createCard(card) {
     return `
-      <img data-pairId="${card.id}" class="hidden icon" src="${card.icon}">
+      <img onclick="showPicture(this)" data-pairId="${card.id}" class="hidden icon" src="${card.icon}">
     `;
   }
+
+  // massiiv paaris kaartide jaoks
+  let pairs = [];
+
+  // Mängu loogika
+
+  let card1 = null;
+  let card2 = null;
+  
+  function showPicture(element) {
+    element.classList.remove('hidden');
+    element.classList.add('visible');
+  
+    if (!card1) {
+      card1 = element;
+    } else {
+      card2 = element;
+      let result = checkCard(card1, card2);
+      if (result) {
+        card1 = null;
+        card2 = null;
+      } else {
+     
+        setTimeout(function() { 
+          if (card1 !== null && card2 !== null) {
+            card1.classList.remove('visible');
+            card1.classList.add('hidden');
+            card2.classList.remove('visible');
+            card2.classList.add('hidden');
+            card1 = null;
+            card2 = null;
+          }
+        }, 500);
+      }
+    }
+  
+    element = null;
+  }
+  
+  // Check for a pair function
+  function checkCard(card1, card2) {
+    if (card1.getAttribute("data-pairId") === card2.getAttribute("data-pairId")) {
+      card1.dataset.pairMatch = "match";
+      card2.dataset.pairMatch = "match";
+      card1.removeAttribute("onclick");
+      card2.removeAttribute("onclick");
+      pairs.push(card1);
+      pairs.push(card2);
+      console.log(pairs)
+      return true;
+    } else {
+      return false;
+    }
+  } 
 
   // Kaardid ruudustikku
 function displayCards() {
